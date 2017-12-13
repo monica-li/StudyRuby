e8daa812af7ce09bdc8c7e307cee33e10cef06eb
monica-li <lsp335@hotmail.com>
2017-12-13 18:54:11 +0800

    change git user to monica

--- config/copylog.sh
+++ config/copylog.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
+# this is an example, which needs to be improved to use
 
 locallogpath=/g/HotCloud/logsfromali/test
 serverip=47.92.123.34
 serverlogpath=/usr/local/glassfish4/glassfish/domains/domain1/logs
-logfile=server.log_2017-09-01T09-05-31
+logfile=server.log_2017-09-01*
 serveruser=root
-
 
 if [! -d $locallogpath]; then
  mkdir $locallogpath
