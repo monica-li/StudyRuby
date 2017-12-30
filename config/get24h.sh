71221b9af0b6ea99f5c366947d1bcdbb32fd4878
monica-li <lsp335@hotmail.com>
2017-12-30 16:54:49 +0800

    data from moji

--- config/get24h.sh
+++ config/get24h.sh
@@ -1,85 +1,21 @@
 #!/bin/sh
 
 appCode=
-token=1b89050d9f64191d494c806f78e8ea36
+token=008d2ad9197090c5dddc76f583616606
 
-dclat=39.91488908
-dclon=116.40387397
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$dclat&lon=$dclon&token=$token' >>data24h.json
-
-xclat=
-xclon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$xclat&lon=$xclon&token=$token' >>data24h.json
-
-cylat=
-cylon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$cylat&lon=$cylon&token=$token' >>data24h.json
-
-ftlat=
-ftlon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$ftlat&lon=$ftlon&token=$token' >>data24h.json
-
-sjslat=
-sjslon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$sjslat&lon=$sjslon&token=$token' >>data24h.json
-
-hdlat=
-hdlon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$hdlat&hdlon=$lon&token=$token' >>data24h.json
-
-mtglat=
-mtglon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$mtglat&lon=$mtglon&token=$token' >>data24h.json
-
-fslat=
-fslon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$fslat&fslon=$lon&token=$token' >>data24h.json
-
-tzlat=
-tzlon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$tzlat&lon=$tzlon&token=$token' >>data24h.json
-
-sylat=
-sylon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$sylat&lon=$sylon&token=$token' >>data24h.json
-
-cplat=
-cplon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$cplat$lon=$cplon&token=$token' >>data24h.json
-
-dxlat=
-dxlon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$dxlat&lon=$dxlon&token=$token' >>data24h.json
-
-hrlat=
-hrlon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$hrlat&lon=$hrlon&token=$token' >>data24h.json
-
-pglat=
-pglon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$pglat&lon=$pglon&token=$token' >>data24h.json
-
-mylat=
-mylon=
-
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$mylat&lon=$mylon&token=$token' >>data24h.json
-
-yqlat=
-yqlon=
-curl -i -X POST 'http://aliv8.data.moji.com/whapi/json/aliweather/forecast24hours'  -H 'Authorization:APPCODE $appCode' --data 'lat=$yqlat&lon=$yqlon&token=$token' >>data24h.json
-
-
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=284609&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=284610&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=3&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=4&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=5&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=6&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=7&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=8&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=9&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=10&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=11&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=12&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=13&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=14&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=15&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
+curl -i -X POST "http://aliv18.data.moji.com/whapi/json/alicityweather/forecast24hours"  -H "Authorization:APPCODE $appCode" --data "cityId=22&token=008d2ad9197090c5dddc76f583616606" >>data24h.json
