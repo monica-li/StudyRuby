24ba42f9cef79d5083333c1c95c21fb4b6d5c0ce
monica-li <lsp335@gmail.com>
2018-01-04 14:56:03 +0800

    fix

--- spec/monitor/add_24h_beijing_weather_moji_spec.rb
+++ spec/monitor/add_24h_beijing_weather_moji_spec.rb
@@ -7,7 +7,7 @@ describe "add multiple temperatures" do
 
   it "add 24 hours weather information of Beijing 16 districts from moji" do
 	f=File.dirname(__FILE__) + '/moji24h-beijing.json'
-	auth={:host=>"47.92.123.34", :port=>"8080", :reststr=>"api/private"}
+	auth={:host=>"47.92.123.34", :port=>"8080", :reststr=>"private"}
 	MonitorWeatherEditor.add_24hour_weather_from_moji_by_rest(auth,f)
 #	MonitorWeatherEditor.add_24hour_weather_from_moji(f)	
   end
