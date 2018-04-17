f7af31bf4994131e396f57659c6727f1bfc554de
Lsp2217459 <lsp335@hotmail.com>
2018-04-17 17:54:05 +0800

    test

--- MonicaAF/installProduct.sh
+++ MonicaAF/installProduct.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 installResult=1
 
-echo "args from runAF.sh: $productname, $version, $build"
+productname="testprod"
+version="1.1"
+build="1.1.2"
+distripath="/Users/monica-li/$productname/$version/$build"
+
+productpath="/Users/monica-li/opt/$productname"
+installfile="test.tar"
+
+if [ ! -d "$productpath" ]; then
+	mkdir $productpath
+fi
+if [ ! -d "$productpath/$version" ]; then
+	mkdir $productpath/$version
+fi
+if [ ! -d "$productpath/$version/$build" ]; then
+	mkdir $productpath/$version/$build
+fi
+
+pushd $productpath/$version/$build
+tar zxvf $distripath/$installfile
+
+configfile="configure.xml"
+sed -i 's#baseDir="\${CATALINA_HOME}\/conf#baseDir="'$productpath'\/'$version'\/'$build'\/conf#' $configfile
+
+export JAVA_HOME=/usr/bin/java
+pushd $productpath/$version/$build/content/tomcat/bin
+sh startup.sh
